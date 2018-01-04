e3c93fd202fa508c48abf2d48e25d9119ae1e6af
Evgenii Klimov <disturh@gmail.com>
2018-01-04 17:12:56 +0300

    initial

---
+++ source.js
@@ -0,0 +1,23 @@
+function newClass(Constructor){
+    Constructor = Constructor || function newClass(){};
+    for (var i=1; i < arguments.length; i++) {
+        var method = arguments[i];
+        var methodName = getFunctionName(method);
+        Constructor.prototype[methodName] = method;
+    }
+    return Constructor;
+}
+function extend(ParentConstructor, Constructor){
+    function F(){}; // empty Constructor to avoid ParentConstructor running
+    F.prototype = ParentConstructor.prototype;
+    Constructor.prototype = new F();
+    Constructor.prototype.constructor = Constructor;
+
+    for (var i=2; i < arguments.length; i++) {
+        var method = arguments[i];
+        var methodName = getFunctionName(method);
+        Constructor.prototype[methodName] = method;
+    }
+    return Constructor;
+}
+function getFunctionName(f){ return f.toString().match(/^function\s*([^\s(]+)/)[1]; }
